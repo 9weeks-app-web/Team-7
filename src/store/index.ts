da26bc5ebe0b5b830d79929b888ebbd86c09339a
wantop1 <wantop@jbnu.ac.kr>
2023-12-29 14:20:09 +0900

    Feat: authSlice 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./counterSlice";
+import authSlice from "./authSlice";
 
 const store = configureStore({
   reducer: {
     counter: counterSlice,
+    auth: authSlice,
   },
 });
 
