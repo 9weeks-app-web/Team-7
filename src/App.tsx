217cc1dbd562287cf540187edbfcb24a9f0602a5
wantop1 <wantop@jbnu.ac.kr>
2023-12-29 14:38:01 +0900

    Feat: 로그인 페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Home from "./pages/Home";
 import Layout from "./components/layout/Layout";
 import NotFound from "./pages/NotFound";
+import Auth from "./pages/Auth";
+import { RootState } from "./store";
 
 const App = (): JSX.Element => {
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route
+          path="login"
+          element={isAuthenticated ? <Navigate to="/" /> : <Auth />}
+        />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
