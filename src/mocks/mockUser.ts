9873c223399603382b8fcf13549903e51b47b449
wantop1 <wantop@jbnu.ac.kr>
2023-12-29 14:19:21 +0900

    Feat: mockUser 추가

---
+++ src/mocks/mockUser.ts
@@ -0,0 +1,9 @@
+import { LoginUser } from "../types/loginTypes";
+
+export const MOCK_USERS: LoginUser[] = [
+  { email: "admin@sfacfolio.com", password: "sfacfolio7!" },
+  { email: "support@sfacfolio.com", password: "sfacfolio7!" },
+  { email: "sales@sfacfolio.com", password: "sfacfolio7!" },
+  { email: "hr@sfacfolio.com", password: "sfacfolio7!" },
+  { email: "contact@sfacfolio.com", password: "sfacfolio7!" },
+];
