f141cf2d0c0f3d93a6bf1e2103c7eeaa5b808e01
wantop1 <wantop@jbnu.ac.kr>
2024-01-03 15:15:48 +0900

    Feat: password recovery mock user 추가

--- src/mocks/mockUser.ts
+++ src/mocks/mockUser.ts
@@ -1,4 +1,5 @@
 import { LoginUser } from "../types/loginTypes";
+import { PasswordRecoveryUser } from "../types/passwordTypes";
 
 export const MOCK_USERS: LoginUser[] = [
   { email: "admin@sfacfolio.com", password: "sfacfolio7!" },
@@ -6,4 +7,32 @@ export const MOCK_USERS: LoginUser[] = [
   { email: "sales@sfacfolio.com", password: "sfacfolio7!" },
   { email: "hr@sfacfolio.com", password: "sfacfolio7!" },
   { email: "contact@sfacfolio.com", password: "sfacfolio7!" },
+];
+
+export const MOCK_PASSWORD_RECOVERY_USERS: PasswordRecoveryUser[] = [
+  {
+    name: "admin",
+    email: "admin@sfacfolio.com",
+    phoneNumber: "010-1234-5678",
+  },
+  {
+    name: "support",
+    email: "support@sfacfolio.com",
+    phoneNumber: "010-1234-5678",
+  },
+  {
+    name: "sales",
+    email: "sales@sfacfolio.com",
+    phoneNumber: "010-1234-5678",
+  },
+  {
+    name: "hr",
+    email: "hr@sfacfolio.com",
+    phoneNumber: "010-1234-5678",
+  },
+  {
+    name: "contact",
+    email: "contact@sfacfolio.com",
+    phoneNumber: "010-1234-5678",
+  },
 ];
