a4e6b353ac2f05adb4add7997f9d77f428dea417
Hyein Jung <penguin636@naver.com>
2024-01-05 13:46:28 +0900

    Feat: portfolioBox, List 구현

---
+++ src/types/portfolioBoxTypes.ts
@@ -0,0 +1,11 @@
+export interface PortfolioBoxProps {
+  thumbnail: string;
+  title: string;
+  profile: string;
+  userName: string;
+  like: number;
+  view: number;
+  badge_premium: boolean;
+  badge_type: string;
+  badge_pick: boolean;
+}
