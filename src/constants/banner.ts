26fae1db5d3282808d06ad217fdb1121a9da9be7
minzyee <vvzmsqufvv@gmail.com>
2024-01-05 15:10:53 +0900

    Refactor: 가독성을 위한 컴포넌트 분리

---
+++ src/constants/banner.ts
@@ -0,0 +1,42 @@
+export const banners = [
+  {
+    id: 1,
+    img: "pcEventBanner1",
+    text: {
+      title: "SFAC",
+      context: "컨텐츠",
+    },
+  },
+  {
+    id: 2,
+    img: "pcEventBanner2",
+    text: {
+      title: "SFAC",
+      context: "컨텐츠",
+    },
+  },
+  {
+    id: 3,
+    img: "pcEventBanner3",
+    text: {
+      title: "",
+      context: "",
+    },
+  },
+  {
+    id: 4,
+    img: "pcEventBanner4",
+    text: {
+      title: "당신의 상상을 현실로!",
+      context: "컨텐츠",
+    },
+  },
+  {
+    id: 5,
+    img: "pcEventBanner5",
+    text: {
+      title: "SFAC",
+      context: "컨텐츠",
+    },
+  },
+];
