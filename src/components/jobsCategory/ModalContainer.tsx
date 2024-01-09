a4032f13a7f6b763738eb687e92e24388913fffb
minzyee <vvzmsqufvv@gmail.com>
2024-01-09 19:55:00 +0900

    Style: 텍스트 추가

--- src/components/jobsCategory/ModalContainer.tsx
+++ src/components/jobsCategory/ModalContainer.tsx
@@ -16,6 +16,12 @@ const ModalContainer = () => {
 
   return (
     <>
+      <h2 className="text-[24px] text-neutral-100 font-semibold mb-[20px]">
+        포트폴리오
+      </h2>
+      <span className="text-[16px] text-neutral-40">
+        직무별 카테고리 및 필터
+      </span>
       <CategoryListSlide handleOpenModal={handleOpenModal} />
       <ModalPopup>
         {isOpenModal && (
