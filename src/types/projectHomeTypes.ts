6b724e334475e062fbfacc5d47c9e51d847ce6c7
minzyee <vvzmsqufvv@gmail.com>
2024-01-11 13:13:41 +0900

    Feat: 프로젝트 홈 페이지 화면 구현

---
+++ src/types/projectHomeTypes.ts
@@ -0,0 +1,12 @@
+export interface ProjectHomeCardProps {
+  thumbnail: string;
+  title: string;
+  deadline: number;
+  groupmembers: number;
+  recruitment: number;
+  hashtag: string[];
+  context: string;
+  liked: number;
+  bookmark: number;
+  teamMembers: string[];
+}
