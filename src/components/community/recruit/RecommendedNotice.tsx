b0d4d13c7a9531508519c8c63b6308f24979762d
wantop1 <wantop@jbnu.ac.kr>
2024-01-11 15:43:00 +0900

    Feat: 커뮤니티 채용 공고 탭 컴포넌트 추가

---
+++ src/components/community/recruit/RecommendedNotice.tsx
@@ -0,0 +1,329 @@
+const RecommendedNotice = () => {
+  return (
+    <div className="mt-[28px] flex flex-col items-center justify-center gap-[40px] p-[30px] bg-[#fff] border-[1px] border-solid border-[#e6e6e6] rounded-[6px]">
+      <div className="flex flex-row items-start justify-start gap-[20px]">
+        <div className="flex flex-row items-center justify-start">
+          <div className="flex flex-row items-start justify-start">
+            <div className="flex flex-col items-start justify-start gap-[16px] bg-[#fff]">
+              <div className="relative w-[270px] h-[151px] shrink-0 flex">
+                <div className="absolute left-0 top-0 w-[270px] h-[151px] bg-[#fff] border-[1px] border-solid border-[#e6e6e6] rounded-[6px]"></div>
+                <img
+                  className="absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2"
+                  width="198"
+                  height="35"
+                  src="/community/myrealtrip.png"
+                  alt="myrealtrip"
+                ></img>
+              </div>
+              <div className="flex flex-col items-start justify-start gap-[10px]">
+                <div className="flex flex-col items-start justify-start gap-[6px]">
+                  <div className="flex flex-row items-start justify-start">
+                    <div className="text-[16px] leading-[140%]  text-[#1a1a1a] text-center whitespace-nowrap">
+                      (주)마이리얼트립
+                    </div>
+                  </div>
+                  <div className="flex flex-col items-start justify-start">
+                    <div className="text-[20px]  font-semibold text-[#1a1a1a] whitespace-nowrap">
+                      프로덕트 디자이너 채용
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-row items-start justify-start">
+                  <div className="h-[13px] flex flex-row items-center justify-center pt-0 pr-[8px] pb-0 pl-0 bg-[#fff]">
+                    <div className="text-[14px] leading-[140%]  font-medium text-[#4d4d4d] text-center whitespace-nowrap">
+                      서울시 서초구
+                    </div>
+                  </div>
+                  <div className="h-[13px] flex flex-row items-center justify-center py-0 px-[8px] bg-[#fff]  border-#e6e6e6 border-x border-[0_0_0_1px]">
+                    <div className="text-[14px] leading-[140%]  font-medium text-[#333] text-center whitespace-nowrap">
+                      경력・신입
+                    </div>
+                  </div>
+                  <div className="h-[13px] flex flex-row items-center justify-center py-0 px-[8px] bg-[#fff] border-#e6e6e6 border-r border-[0_0_0_1px]">
+                    <div className="text-[14px] leading-[140%]  font-medium text-[#4d4d4d] text-center whitespace-nowrap">
+                      정규직
+                    </div>
+                  </div>
+                </div>
+                <div className="text-[14px] leading-[140%]  font-medium text-[#4c8bff] whitespace-nowrap">
+                  ~01.12 (금)・채용시 마감
+                </div>
+                <div className="flex flex-row items-start justify-start gap-[10px]">
+                  <div className="flex flex-row items-center justify-center py-[8px] px-[10px] border-[1px] border-solid border-[#e6e6e6] rounded-[4px] overflow-hidden">
+                    <div className="text-[10px] leading-[140%]  font-semibold text-[#808080] whitespace-nowrap">
+                      🎁 취업 축하금
+                    </div>
+                  </div>
+                  <div className="flex flex-row items-center justify-center py-[8px] px-[10px] border-[1px] border-solid border-[#e6e6e6] rounded-[4px] overflow-hidden">
+                    <div className="text-[10px] leading-[140%]  font-semibold text-[#808080] whitespace-nowrap">
+                      👥 유연 근무제
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-start">
+          <div className="flex flex-row items-start justify-start">
+            <div className="flex flex-col items-start justify-start gap-[16px] bg-[#fff]">
+              <div className="relative w-[270px] h-[151px] shrink-0 flex">
+                <img
+                  className="absolute left-0 top-0"
+                  width="270"
+                  height="151"
+                  src="/community/everytime.png"
+                  alt="everytime"
+                ></img>
+              </div>
+              <div className="flex flex-col items-start justify-start gap-[10px]">
+                <div className="flex flex-col items-start justify-start gap-[6px]">
+                  <div className="flex flex-row items-start justify-start">
+                    <div className="text-[16px] leading-[140%]  text-[#1a1a1a] text-center whitespace-nowrap">
+                      (주)에브리타임
+                    </div>
+                  </div>
+                  <div className="flex flex-col items-start justify-start">
+                    <div className="text-[20px]  font-semibold text-[#1a1a1a] whitespace-nowrap">
+                      UXUI 디자이너 채용
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-row items-start justify-start">
+                  <div className="h-[13px] flex flex-row items-center justify-center pt-0 pr-[8px] pb-0 pl-0 bg-[#fff]">
+                    <div className="text-[14px] leading-[140%]  font-medium text-[#4d4d4d] text-center whitespace-nowrap">
+                      서울시 서초구
+                    </div>
+                  </div>
+                  <div className="h-[13px] flex flex-row items-center justify-center py-0 px-[8px] bg-[#fff]  border-#e6e6e6 border-x border-[0_0_0_1px]">
+                    <div className="text-[14px] leading-[140%]  font-medium text-[#4d4d4d] text-center whitespace-nowrap">
+                      경력 (5년 이상)
+                    </div>
+                  </div>
+                  <div className="h-[13px] flex flex-row items-center justify-center py-0 px-[8px] bg-[#fff]  border-#e6e6e6 border-x border-[0_0_0_1px]">
+                    <div className="text-[14px] leading-[140%]  font-medium text-[#4d4d4d] text-center whitespace-nowrap">
+                      정규직
+                    </div>
+                  </div>
+                </div>
+                <div className="text-[14px] leading-[140%]  font-medium text-[#4c8bff] whitespace-nowrap">
+                  ~01.05 (금)・채용시 마감
+                </div>
+                <div className="flex flex-row items-start justify-start gap-[10px]">
+                  <div className="flex flex-row items-center justify-center py-[8px] px-[10px] border-[1px] border-solid border-[#e6e6e6] rounded-[4px] overflow-hidden">
+                    <div className="text-[10px] leading-[140%]  font-semibold text-[#808080] whitespace-nowrap">
+                      🎁 취업 축하금
+                    </div>
+                  </div>
+                  <div className="flex flex-row items-center justify-center py-[8px] px-[10px] border-[1px] border-solid border-[#e6e6e6] rounded-[4px] overflow-hidden">
+                    <div className="text-[10px] leading-[140%]  font-semibold text-[#808080] whitespace-nowrap">
+                      👥 유연 근무제
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-start">
+          <div className="flex flex-row items-start justify-start">
+            <div className="flex flex-col items-start justify-start bg-[#fff]">
+              <div className="flex flex-col items-start justify-start gap-[10px]">
+                <div className="relative w-[270px] h-[151px] shrink-0 flex">
+                  <div className="absolute left-0 top-0 w-[270px] h-[151px] bg-[#fff] border-[1px] border-solid border-[#e6e6e6] rounded-[6px]"></div>
+                  <img
+                    className="absolute w-full h-full"
+                    src="/community/daangn.png"
+                    alt="daangn"
+                  ></img>
+                </div>
+                <div className="flex flex-col items-start justify-start gap-[6px]">
+                  <div className="flex flex-row items-start justify-start">
+                    <div className="text-[16px] leading-[140%]  text-[#1a1a1a] text-center whitespace-nowrap">
+                      (주) 당근
+                    </div>
+                  </div>
+                  <div className="flex flex-col items-start justify-start">
+                    <div className="w-[270px] text-[20px]  font-semibold text-[#1a1a1a]">
+                      당근마켓 23년 전직군 정규직 채용
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-row items-start justify-start">
+                  <div className="h-[13px] flex flex-row items-center justify-center pt-0 pr-[8px] pb-0 pl-0 bg-[#fff]">
+                    <div className="text-[14px] leading-[140%]  font-medium text-[#4d4d4d] text-center whitespace-nowrap">
+                      서울시 서초구
+                    </div>
+                  </div>
+                  <div className="h-[13px] flex flex-row items-center justify-center py-0 px-[8px] bg-[#fff]  border-#e6e6e6 border-x border-[0_0_0_1px]">
+                    <div className="text-[14px] leading-[140%]  font-medium text-[#4d4d4d] text-center whitespace-nowrap">
+                      경력 (5년 이상)
+                    </div>
+                  </div>
+                  <div className="h-[13px] flex flex-row items-center justify-center py-0 px-[8px] bg-[#fff] border-#e6e6e6 border-x border-[0_0_0_1px]">
+                    <div className="text-[14px] leading-[140%]  font-medium text-[#4d4d4d] text-center whitespace-nowrap">
+                      정규직
+                    </div>
+                  </div>
+                </div>
+                <div className="text-[14px] leading-[140%]  font-medium text-[#4c8bff] whitespace-nowrap">
+                  ~01.05 (금)・채용시 마감
+                </div>
+                <div className="flex flex-row items-start justify-start">
+                  <div className="flex flex-row items-center justify-center py-[8px] px-[10px] border-[1px] border-solid border-[#e6e6e6] rounded-[4px] overflow-hidden">
+                    <div className="text-[10px] leading-[140%]  font-semibold text-[#808080] whitespace-nowrap">
+                      🎁 취업 축하금
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-start">
+          <div className="flex flex-row items-start justify-start">
+            <div className="flex flex-col items-start justify-start bg-[#fff]">
+              <div className="flex flex-col items-start justify-start gap-[10px]">
+                <div className="relative w-[270px] h-[151px] shrink-0 flex">
+                  <img
+                    className="absolute left-0 top-0"
+                    width="270"
+                    height="151"
+                    src="/community/pillyze.png"
+                    alt="pillyze"
+                  ></img>
+                </div>
+                <div className="flex flex-col items-start justify-start gap-[6px]">
+                  <div className="flex flex-row items-start justify-start">
+                    <div className="text-[16px] leading-[140%]  text-[#1a1a1a] text-center whitespace-nowrap">
+                      (주)필라이즈
+                    </div>
+                  </div>
+                  <div className="flex flex-col items-start justify-start">
+                    <div className="w-[270px] text-[20px] leading-[140%]  font-semibold text-[#1a1a1a]">
+                      Product Designer 채용
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-row items-start justify-start">
+                  <div className="h-[13px] flex flex-row items-center justify-center pt-0 pr-[8px] pb-0 pl-0 bg-[#fff]">
+                    <div className="text-[14px] leading-[140%]  font-medium text-[#4d4d4d] text-center whitespace-nowrap">
+                      서울시 중구
+                    </div>
+                  </div>
+                  <div className="h-[13px] flex flex-row items-center justify-center py-0 px-[8px] bg-[#fff] border-#e6e6e6 border-x border-[0_0_0_1px]">
+                    <div className="text-[14px] leading-[140%]  font-medium text-[#4d4d4d] text-center whitespace-nowrap">
+                      경력 무관
+                    </div>
+                  </div>
+                  <div className="h-[13px] flex flex-row items-center justify-center py-0 px-[8px] bg-[#fff]  border-#e6e6e6 border-r border-[0_0_0_1px]">
+                    <div className="text-[14px] leading-[140%]  font-medium text-[#4d4d4d] text-center whitespace-nowrap">
+                      정규직
+                    </div>
+                  </div>
+                </div>
+                <div className="text-[14px] leading-[140%]  font-medium text-[#4c8bff] whitespace-nowrap">
+                  상시채용
+                </div>
+                <div className="flex flex-row items-start justify-start gap-[10px]">
+                  <div className="flex flex-row items-center justify-center py-[8px] px-[10px] border-[1px] border-solid border-[#e6e6e6] rounded-[4px] overflow-hidden">
+                    <div className="text-[10px] leading-[140%]  font-semibold text-[#808080] whitespace-nowrap">
+                      👔 사수 O
+                    </div>
+                  </div>
+                  <div className="flex flex-row items-center justify-center py-[8px] px-[10px] border-[1px] border-solid border-[#e6e6e6] rounded-[4px] overflow-hidden">
+                    <div className="text-[10px] leading-[140%]  font-semibold text-[#808080] whitespace-nowrap">
+                      👥 유연 근무제
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-start gap-[10px]">
+        <div className="w-[32px] h-[32px] shrink-0 flex flex-row items-center justify-center rounded-[100px]">
+          <div className="relative w-[32px] h-[32px] shrink-0 flex">
+            <div className="absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 w-[32px] h-[32px] bg-[#fff] border-[1px] border-solid border-[#f3f3f3] rounded-[100px]"></div>
+            <svg
+              width="33"
+              height="32"
+              viewBox="0 0 33 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                x="-0.5"
+                y="0.5"
+                width="31"
+                height="31"
+                rx="15.5"
+                transform="matrix(-1 0 0 1 31.5 0)"
+                fill="white"
+              />
+              <rect
+                x="-0.5"
+                y="0.5"
+                width="31"
+                height="31"
+                rx="15.5"
+                transform="matrix(-1 0 0 1 31.5 0)"
+                stroke="#F3F3F3"
+              />
+              <path
+                d="M19.5 23L12.5 16L19.5 9"
+                stroke="#CCDEFF"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="text-[16px] leading-[140%]  font-medium whitespace-nowrap">
+          <span className="text-[#196aff]">UXUI, 프로덕트 디자인</span>
+          <span className="text-[#808080]"> 채용 정보 </span>
+          <span className="text-[#1a1a1a]">1/5</span>
+        </div>
+        <div className="relative w-[32px] h-[32px] shrink-0 rounded-[100px]">
+          <svg
+            width="33"
+            height="32"
+            viewBox="0 0 33 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="0.5"
+              y="-0.5"
+              width="31"
+              height="31"
+              rx="15.5"
+              transform="matrix(1 0 0 -1 0.5 31)"
+              fill="white"
+            />
+            <rect
+              x="0.5"
+              y="-0.5"
+              width="31"
+              height="31"
+              rx="15.5"
+              transform="matrix(1 0 0 -1 0.5 31)"
+              stroke="#99BDFF"
+            />
+            <path
+              d="M14.5 9L21.5 16L14.5 23"
+              stroke="#196AFF"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecommendedNotice;
