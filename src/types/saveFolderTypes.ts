3a97bab2f8a8df1cb3227f42973ae7ff8c29f1e2
Hyein Jung <penguin636@naver.com>
2024-01-11 15:57:05 +0900

    Feat: 디테일 페이지 저장팝업 추가

--- src/types/saveFolderTypes.ts
+++ src/types/saveFolderTypes.ts
@@ -2,3 +2,8 @@ export interface saveFolderProps {
   thumnail: string;
   title: string;
 }
+
+export interface SavePopupProps {
+  isPopupOpen: (isOpen: boolean) => void;
+  onSelectFolder: (folder: saveFolderProps) => void;
+}
